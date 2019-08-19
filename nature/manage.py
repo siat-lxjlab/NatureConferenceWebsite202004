2c2c617fd5fb95f394a089f5c9e2ac9913b028e7
unknown <1051969307@qq.com>
2019-08-19 10:17:06 +0800

    complete the part of person info update of myconsole

--- nature/manage.py
+++ nature/manage.py
@@ -1,4 +1,5 @@
 import functools
+import re
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, session
 )
@@ -20,28 +21,86 @@ def load_logged_in_user():
             'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
 
+
 @bp.route('/index')
 @bp.route('/')
 @login_required
 def index():
     return render_template('manage/index.html')
 
-@bp.route('/info')
+
+@bp.route('/info', methods=('GET', 'POST'))
+@login_required
 def person():
-    db = get_db()
+    user_id = session.get('user_id')
+    if request.method == 'POST':
+        username = request.form['username']
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        workplace = request.form['workplace']
+        title = request.form['title']
+        db = get_db()
+        error = None
+
+        # confirm the email format
+        email_pattern = r'^[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+){0,4}@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$'
+        if not username:
+            error = 'Username is required.'
+        elif not name:
+            error = 'Name is required.'
+        elif not re.match(email_pattern, email):
+            error = 'Email format is not right.'
+        elif not (len(phone) == 11 or len(phone) == 10):
+            error = 'Phone number is not right.'
+        elif not workplace:
+            error = 'Workplace is required.'
+        elif not (title != "0"):
+            error = 'Title is required.'
+        elif username == g.user["username"]:
+            error = None  
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)      
+        else:
+            error = None
+
+        if error is None:
+            db.execute(
+                'UPDATE user SET username = ?, name = ?, email = ?, phone = ?, workplace = ?, title = ?' 
+                ' WHERE id = ?',
+                (username, name, email, phone, workplace, title, user_id)
+            )
+            db.commit()
+            g.user = get_db().execute(
+                'SELECT * FROM user WHERE id = ?', (user_id,)
+            ).fetchone()
+            return render_template('manage/person.html')
+
+        flash(error)
     return render_template('manage/person.html')
 
+
 @bp.route('/fee')
+@login_required
 def pay():
     db = get_db()
+    # db.execute()
     return render_template('manage/fee.html')
 
+
 @bp.route('/submit')
+@login_required
 def submit():
     db = get_db()
+    # db.execute()
     return render_template('manage/submit.html')
 
+
 @bp.route('/registration')
+@login_required
 def update_registration():
     db = get_db()
+    # db.execute()
     return render_template('manage/registration.html')
